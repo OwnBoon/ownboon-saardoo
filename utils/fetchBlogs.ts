cc10b3c5bd9678c1857ca45e6282053f635ce4cc
SaarD00ishuman <97211377+SaarD00ishuman@users.noreply.github.com>
2023-04-02 16:15:22 +0530

    buth

--- utils/fetchBlogs.ts
+++ utils/fetchBlogs.ts
@@ -1,7 +1,7 @@
 import {  Posts, User } from '../typings'
 
 export const fecthBlogs = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getBlog`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getBlog`)
 
   const data = await res.json()
   const posts: Posts[] = data.posts
