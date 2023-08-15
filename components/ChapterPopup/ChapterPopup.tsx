31315a55a80f0783c6dd9475c096de3fc0255b3f
lojith <lojithvinsuka1998@gmail.com>
2023-08-15 23:52:52 +0530

    remove useless

--- components/ChapterPopup/ChapterPopup.tsx
+++ components/ChapterPopup/ChapterPopup.tsx
@@ -6,9 +6,6 @@ const Dialog = ({ isOpen, onClose, children }: any) => {
     const dialogRef = useRef<any>();
 
     const handleClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        console.log(e.currentTarget.id)
-
-
         if (dialogRef.current && !dialogRef.current.contains(e.target)) {
             onClose(false);
         }
@@ -19,12 +16,6 @@ const Dialog = ({ isOpen, onClose, children }: any) => {
             <div className="fixed inset-0 bg-black opacity-50"></div>
             <div ref={dialogRef} className="popupArea z-50 w-full max-w-md p-6 mx-auto bg-gray-900 rounded-[7px] border border-slate-800 border-opacity-75 shadow-lg">
                 {children}
-                {/* <button
-                    className="mt-4 px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring"
-                    onClick={() => onClose(false)}
-                >
-                    Close
-                </button> */}
             </div>
         </div>
     );
