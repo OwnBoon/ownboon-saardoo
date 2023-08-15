938e61c131226a2a9970a0275cba167141217e3d
SaarD00 <95203175+SaarD00@users.noreply.github.com>
2023-08-15 19:28:59 +0530

    Update roadmaps.tsx

--- pages/roadmaps.tsx
+++ pages/roadmaps.tsx
@@ -6,7 +6,7 @@ import Sidebar from "../components/dashboard/Sidebar";
 import Navbar from "../components/Navbar";
 import { GetServerSideProps } from "next";
 import { fetchUsers } from "../utils/fetchUsers";
-import { Goals, Notes, User } from "../typings";
+import { Goals, Notes, User, UserBody } from "../typings";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
@@ -25,6 +25,7 @@ import {
   Row,
   Input,
   Modal,
+  Loading,
 } from "@nextui-org/react";
 import { Text } from "@nextui-org/react";
 import {
@@ -34,16 +35,11 @@ import {
 } from "@heroicons/react/24/outline";
 import { MdPassword } from "react-icons/md";
 import ReactPlayer from "react-player";
-import { fetchRoadmaps } from "../utils/fetchRoadmap";
+import Head from "next/head";
+import { categories } from "../utils/constants";
 interface datatype {
   message: {
-    choices: [
-      {
-        message: {
-          content: string;
-        };
-      }
-    ];
+    response: string;
   };
 }
 interface Info {
@@ -54,7 +50,6 @@ interface Props {
   users: User[];
   goals: Goals[];
   notes: Notes[];
-  roadmaps: Roadmaps[];
 }
 
 interface RoadmapItem {
@@ -63,7 +58,7 @@ interface RoadmapItem {
   parent: number;
   title: string;
 }
-const Home = ({ users, goals, notes, roadmaps }: Props) => {
+const Home = ({ users, goals, notes }: Props) => {
   const { isLoaded, isSignedIn, user } = useUser();
   const [show, setShow] = useState(false);
   const router = useRouter();
@@ -109,15 +104,26 @@ const Home = ({ users, goals, notes, roadmaps }: Props) => {
   const [visible, setVisible] = useState(false);
   const [text, setText] = useState("");
   const [stuff, setStuff] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const match = users.filter(
+    (userss) => userss.email === user?.emailAddresses[0].emailAddress
+  );
   const handler = async (texts: string) => {
     setText(texts);
-    const result = await fetch(`/api/roadmap/normalchat?title=${text}`);
+    setLoading(true);
+    const result = await fetch(
+      `/api/roadmap/normalchat?title=${text}?categories=${categories.map(
+        (cateogr) => cateogr.name
+      )}`
+    );
     const json = await result.json();
     // @ts-ignore
     // const deez = stuff.message.choices[0].message.content;
     // const json1 = JSON.parse(deez);
     setStuff(json);
     setVisible(true);
+    setLoading(false);
     return json;
   };
 
@@ -129,14 +135,24 @@ const Home = ({ users, goals, notes, roadmaps }: Props) => {
 
     setShow(true);
 
-    const result = await fetch(
-      `https://nodejs-sms.saard00vfx.repl.co/api/handler?title=${desc}`
-    );
+    const result = await fetch(`/api/roadmap/generate?title=${desc}`);
 
     const json = await result.json();
     setData(json);
+    console.log(json);
+    setShow(false);
     return json;
   };
+
+  if (data) {
+    const index = data.message.response.indexOf("[");
+
+    // Extract the substring starting from the third line
+    const result = data.message.response.substring(index);
+
+    console.log(result);
+  }
+
   const [modaldata, setModaldata] = useState<Info>();
   useEffect(() => {
     if (stuff) {
@@ -152,6 +168,10 @@ const Home = ({ users, goals, notes, roadmaps }: Props) => {
   if (!data) {
     return (
       <div>
+        <Head>
+          <title>Self Help Hub</title>
+          <link rel="icon" href="/logo.png" />
+        </Head>
         <div className="grid grid-cols-12 bg-[#f4f1eb]/50">
           <Sidebar />
           <div className="col-span-10">
@@ -224,13 +244,37 @@ const Home = ({ users, goals, notes, roadmaps }: Props) => {
   // const roadmap = dataObject.roadmap;
   if (data) {
     // @ts-ignore
-    const roadmapdata = data?.message.choices[0].message.content;
-    const fine = roadmapdata.replace("@finish", "");
-    const sus = JSON.parse(fine);
+    const roadmapdata = data?.message.response.roadmap;
+    // const fine = roadmapdata.replace("@finish", "");
+    // const sus = JSON.parse(fine);
+    // console.log(sus);
     // console.log(roadmapdata);
+
+    const addCategory = async (name: string) => {
+      try {
+        const postInfo: UserBody = {
+          id: match[0]._id,
+          categories: match[0].categories + "," + name,
+        };
+        const result = await fetch(`/api/addCategory`, {
+          body: JSON.stringify(postInfo),
+          method: "POST",
+        });
+        const json = await result.json();
+        return json;
+      } catch (err) {
+        console.error(err);
+      } finally {
+        window.location.reload();
+      }
+    };
 
     return (
       <div className="grid grid-cols-12 bg-[#f4f1eb]/50">
+        <Head>
+          <title>Self Help Hub - {text}</title>
+          <link rel="icon" href="/logo.png" />
+        </Head>
         <Sidebar />
         <div className="col-span-10">
           {" "}
@@ -246,7 +290,7 @@ const Home = ({ users, goals, notes, roadmaps }: Props) => {
               >
                 Welcome to Self Help Hub
               </Text>
-              <Grid>
+              <Grid className="flex justify-between gap-10">
                 <Textarea
                   size="xl"
                   cols={50}
@@ -258,18 +302,52 @@ const Home = ({ users, goals, notes, roadmaps }: Props) => {
                   value={desc}
                   labelPlaceholder="Issue description"
                 />
+                {/* <Tooltip content={"click to follow this category"}>
+                  <Button
+                    onPress={() => addCategory(sus.category)}
+                    color="secondary"
+                    shadow
+                    auto
+                  >
+                    {sus.category}
+                  </Button>
+                </Tooltip> */}
               </Grid>
               <Grid className="">
-                <Button onPress={(e) => fetchRoadmap(e)} color="gradient" auto>
+                <Button
+                  shadow
+                  onPress={(e) => fetchRoadmap(e)}
+                  color="gradient"
+                  auto
+                >
                   Submit
                 </Button>
               </Grid>
-              <div></div>
+              {show ? (
+                <Grid className="flex flex-col items-center">
+                  <Progress
+                    indeterminated
+                    value={50}
+                    color="secondary"
+                    status="secondary"
+                  />
+                  <Text color="gray" h2 size={15}>
+                    takes around a minute
+                  </Text>
+                </Grid>
+              ) : null}
             </div>
           </div>
           {/* <DraggableRoadmap data={sampleData} /> */}
           <div className="flex border gap-2 w-full overflow-x-scroll scrollbar scrollbar-none mt-20 ">
-            {sus.roadmap.map((roadmaps: RoadmapItem) => (
+            <div>
+              {loading ? (
+                <Grid>
+                  <Loading type="points" />
+                </Grid>
+              ) : null}
+            </div>
+            {/* {sus.roadmap.map((roadmaps: RoadmapItem) => (
               <div className="flex items-center w-full justify-center">
                 <ArrowRightIcon className="h-5 w-5 " />
                 <Card
@@ -307,7 +385,7 @@ const Home = ({ users, goals, notes, roadmaps }: Props) => {
                   </Modal>
                 </Card>
               </div>
-            ))}
+            ))} */}
           </div>
         </div>{" "}
       </div>
@@ -318,7 +396,6 @@ const Home = ({ users, goals, notes, roadmaps }: Props) => {
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const users = await fetchUsers();
   const goals = await fetchGoals();
-  const roadmaps = await fetchRoadmaps();
   const notes = await fetchNotes();
 
   return {
@@ -326,7 +403,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       users,
       goals,
       notes,
-      roadmaps,
     },
   };
 };
