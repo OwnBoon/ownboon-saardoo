13f20c2583f7d69ba12b56e866b1a275fd589846
lojith <lojithvinsuka1998@gmail.com>
2023-08-18 18:53:31 +0530

    select todos UI

--- components/TodoList/TodoList.tsx
+++ components/TodoList/TodoList.tsx
@@ -278,7 +278,7 @@ const TodoList = ({ todos, user, setTodos }: Props) => {
                                 height={30}
                                 className="p-2 fade transition-all  rounded  drag-handle"
                             />
-                            <div className="border-solid border-gray-700 mb-px relative w-6 shrink-0 h-6 border-2 rounded" />
+                            <input id="default-checkbox" type="checkbox" value="" className="border-solid border-gray-700 bg-transparent mb-px relative w-6 shrink-0 h-6 border-2 rounded checked:bg-[#2CD3E1]" />
                             <div className="whitespace-nowrap  font-sans text-white relative">
                                 {t.title}
                             </div>
