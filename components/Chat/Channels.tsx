961ce8072a9c5a3adb55e43c133db6787f437a1b
DeveshPatel-14 <devpatel1130@gmail.com>
2023-10-01 17:51:27 +0530

    updated chat components

---
+++ components/Chat/Channels.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+//@ts-ignore
+import { useChannelListContext } from '@sendbird/uikit-react/ChannelList/context';
+
+
+//@ts-ignore
+const demo = () => {
+  const {allChannels,currentChannel,channelListQuery,channelSource} = useChannelListContext();
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default demo
---
+++ components/Chat/OpenChannel.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+//@ts-ignore
+import { useOpenChannelContext } from '@sendbird/uikit-react/OpenChannel/context';
+
+
+//@ts-ignore
+const demo = () => {
+  const {allMessages} = useOpenChannelContext();
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default demo
