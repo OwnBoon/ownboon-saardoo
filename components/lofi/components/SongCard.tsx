8b9c2e9add6ccd9c60fbc71433eb8e545954af12
lojith <lojithvinsuka1998@gmail.com>
2023-10-02 08:40:50 +0530

    lofi page UI changes

--- components/lofi/components/SongCard.tsx
+++ components/lofi/components/SongCard.tsx
@@ -19,31 +19,33 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }: any) => {
   };
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <div className="relative w-full h-56 group">
-        <div
-          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title
+    <div className="flex flex-row w-[300px] h-[100px] items-center justify-start gap-3 p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-fit h-[100px] group flex items-center justify-center">
+        {/* <div
+          className={`absolute justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title
             ? "flex bg-black bg-opacity-70"
             : "hidden"
             }`}
         >
-          <PlayPause
-            isPlaying={isPlaying}
-            activeSong={activeSong}
-            song={song}
-            handlePause={handlePauseClick}
-            handlePlay={handlePlayClick}
+
+        </div> */}
+        <PlayPause
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          song={song}
+          handlePause={handlePauseClick}
+          handlePlay={handlePlayClick}
+        >
+          <img
+            alt="song_img"
+            src={song.images?.coverart}
+            className="w-[50px] h-[50px] rounded-lg"
           />
-        </div>
-        <img
-          alt="song_img"
-          src={song.images?.coverart}
-          className="w-full h-full rounded-lg"
-        />
+        </PlayPause>
       </div>
 
       <div className="mt-4 flex flex-col">
-        <p className="font-semibold text-lg text-white truncate">
+        <p className="font-semibold text-sm text-white truncate">
           <Link href={`/songs/${song?.key}`}>{song.title}</Link>
         </p>
         <p className="text-sm truncate text-gray-300 mt-1">
@@ -59,6 +61,7 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }: any) => {
         </p>
       </div>
     </div>
+
   );
 };
 
