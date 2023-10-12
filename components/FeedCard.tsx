df854e76dbeb35c24126adb3d002b5520ce896a3
Dev Gupta <52696089+Devgupta0@users.noreply.github.com>
2023-10-12 13:21:20 +0530

    Blog and Post UI updated as per the figma design
    Blog and Post UI updated as per the figma design

--- components/FeedCard.tsx
+++ components/FeedCard.tsx
@@ -16,6 +16,7 @@ import { BsSend } from "react-icons/bs";
 import { useUser } from "@clerk/nextjs";
 import ReactPlayer from "react-player";
 import { fetchFeedComments } from "../utils/fetchFeedComments";
+import { FaThumbsUp, FaShareAlt, FaExclamationCircle, FaComment } from "react-icons/fa";
 
 interface Props {
   feeds: Videos;
@@ -32,6 +33,7 @@ const FeedCard = ({ feeds }: Props) => {
   };
 
   useEffect(() => {
+    
     refreshComments();
   }, []);
 
@@ -56,29 +58,29 @@ const FeedCard = ({ feeds }: Props) => {
     refreshComments();
   };
   return (
-    <div className="grid shadow-lg h-fit  rounded-lg gap-2 p-0 lg:p-8 pb-12 mb-8 grid-cols-6">
+    <div className="grid blogweekly  rounded-lg gap-2 p-0 lg:p-8 pb-12 mb-8 grid-cols-6">
       <div className=" col-span-4  rounded-lg ">
-        <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+        <div className="relative overflow-hidden shadow-md  mb-6">
           {feeds.video ? (
             <ReactPlayer height={500} controls url={feeds.video} />
           ) : (
             <>
               <img
-                src={feeds.image}
-                className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg"
+                src={"/postimage.png"}
+                className="img-fluid w-full  object-cover" style={{ height: "70vh", borderRadius: "10px" }}
               />
             </>
           )}
         </div>
 
-        <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
-          <div className="flex  justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
+        {/* <div className="block lg:flex text-center items-center justify-center mb-8 w-full"> */}
+        {/* <div className="flex  justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
             <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">
               {feeds.author}
             </p>
-          </div>
-          <div className="font-medium text-gray-700">
-            <svg
+          </div> */}
+        {/* <div className="font-medium text-gray-700"> */}
+        {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6 inline mr-2 text-pink-500"
               fill="none"
@@ -91,55 +93,66 @@ const FeedCard = ({ feeds }: Props) => {
                 strokeWidth="2"
                 d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
               />
-            </svg>
-            <span className="align-middle">
-              {/* @ts-ignore */}
-              <ReactTimeago date={feeds._createdAt} />
-            </span>
-          </div>
-        </div>
-        <div className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
-          {feeds.desc}
-        </div>
+            </svg> */}
+        {/* <span className="align-middle"> */}
+        {/* @ts-ignore */}
+        {/* <ReactTimeago date={feeds._createdAt} /> */}
+        {/* </span> */}
+        {/* </div> */}
+        {/* </div> */}
+
       </div>
-      <div className="col-span-2 border-l px-2">
-        <div className="flex items-center border-b py-1">
-          <User name={feeds.author} src={feeds.profileImage} />
-          <Button size={"xs"} bordered shadow auto>
+      <div className="col-span-2  " style={{ borderLeft: "1px solid rgba(71, 71, 71, 0.50)" }}>
+        <div className="flex items-center  py-1 pb-3" style={{ borderBottom: "1px solid rgba(71, 71, 71, 0.50)" }}>
+          <div className="postprofile pl-2 ">
+            <img src={feeds.profileImage} className="img-fluid" />
+            <span>{feeds.author}</span>
+
+          </div>
+          <Button size={"xs"} className="postfollow">
             Follow
           </Button>
         </div>
-        <div className="p-2">
-          <Text h2 size={15} className="font-semibold">
-            {feeds.author}: <Text className="font-medium">{feeds.title}</Text>
-          </Text>
+        <div className="postdescription mt-2 pl-2">
+          {feeds.desc === undefined ? <><b>codehecker</b> : Lorem ipsum dolor sit amet dolo psum dolo Lorem  adipisc   ipsum dolor... more</> : feeds.desc}
         </div>
-        <div className="space-y-4 mt-5 overflow-y-scroll h-72  shadow-lg rounded-lg p-2">
-          {comments.map((comment) => (
-            <Grid className="flex items-center ">
-              <User
-                name={comment.username}
-                src={comment.profileImg}
-                size={"sm"}
-              />
-              <Text className="text-sm">{comment.comment}</Text>
-            </Grid>
-          ))}
+        {/* <div className="p-2 text-white">
+          <Text h2 size={13} className="font-normal " style={{ color: "white" }}>
+            {feeds.author}.{feeds.title}
+          </Text>
+        </div> */}
+        <div className=" overflow-y-auto  shadow-lg rounded-lg p-2 mt-2" style={{ height: "50vh" }} >
+          {comments.length > 0 ? <>{comments.map((comment) => (
+
+            <div className="postdescriptioncomment  pl-2">
+              <span className="postprofile"><img src={comment.profileImg} className="img-fluid" /></span> <span><b>{comment.username}</b> : {comment.comment}</span>
+            </div>
+          ))}</> : <>
+            <div className="postdescriptioncomment  pl-2">
+              <span className="imagespan"><img src="/commentuser.png" className="img-fluid" />&nbsp;</span> <span><b>Som.Ceo</b> : Lorem ipsum dolor sit amet, elit consectetur adipiscing elit, sed do eiusmod tempor i</span>
+            </div>
+
+
+          </>}
+        </div>
+        <div className="alignbetween pl-3 pt-4">
+          <span><FaThumbsUp className="mr-3 iconsize" /> <FaComment className="mr-3 iconsize" /> <FaShareAlt className="mr-3 iconsize" /></span>
+          <FaExclamationCircle className="iconsize" />
         </div>
         <div className="p-2 outline-none border-none w-full">
           <div className=" flex justify-center gap-2  items-center w-full ">
-            <Input
-              clearable
-              label="Comment"
+            <input
+
+              className="commentinput"
               // @ts-ignore
-              width={900}
+              value={input}
               onChange={(e) => setInput(e.target.value)}
-              placeholder="Write your comment"
+              placeholder="Write your comment..."
             />
             <div
               // @ts-ignore
               onClick={() => handleSubmit(feeds._id)}
-              className="border rounded-full p-2 mt-5 cursor-pointer flex justify-center items-center"
+              className="border rounded-full p-2  cursor-pointer flex justify-center items-center"
             >
               <BsSend className=" w-4 h-4 " />
             </div>
