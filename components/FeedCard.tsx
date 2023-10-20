b1d20436ae02cb53dfab6e640ec3b39488c5d6dc
DeveshPatel-14 <devpatel1130@gmail.com>
2023-10-20 19:28:58 +0530

    Added loader

--- components/FeedCard.tsx
+++ components/FeedCard.tsx
@@ -30,6 +30,8 @@ interface Props {
 
 const FeedCard = ({ feeds }: Props) => {
   const [comments, setComments] = useState<FeedComment[]>([]);
+  const [loading,setLoading] = useState(false);
+
   const { isLoaded, isSignedIn, user } = useUser();
   const refreshComments = async () => {
     // @ts-ignore
@@ -45,6 +47,7 @@ const FeedCard = ({ feeds }: Props) => {
   const [input, setInput] = useState("");
 
   const handleSubmit = async (id: string) => {
+    setLoading(true);
     // Comment logic
     const comment: FeedCommentBody = {
       _type: "feedcomment",
@@ -61,6 +64,7 @@ const FeedCard = ({ feeds }: Props) => {
 
     setInput("");
     refreshComments();
+    setLoading(false);
   };
   return (
     <div className="grid bg-zinc-600 bg-opacity-10 rounded-[10px] border border-zinc-700 border-opacity-50 h-full   gap-2 p-0 lg:p-4 pb-12 mb-8 grid-cols-6">
@@ -103,7 +107,22 @@ const FeedCard = ({ feeds }: Props) => {
               </h2>
             </div>
           </div>
-          <div className="space-y-4 mt-3 overflow-y-scroll h-80 scrollbar-none   shadow-lg rounded-lg p-2 mb-1 border-b-2 border-t-2 border-white/10">
+
+          <div className="space-y-4 mt-3 overflow-y-scroll h-80 scrollbar-none  shadow-lg rounded-lg p-2 mb-1 border-b-2 border-t-2 border-white/10">
+            
+            {loading && <div aria-label="Loading..." role="status" className="ml-[6vw] md:ml-[8vw] lg:ml-[6vw]">
+              <svg
+                className="animate-spin w-6 h-6 fill-blue-500"
+                viewBox="3 3 18 18"
+              >
+                <path
+                  className="opacity-20"
+                  d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
+                ></path>
+                <path d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"></path>
+              </svg>
+            </div>}
+
             {comments.map((comment) => (
               <Grid className="flex items-center gap-4 ">
                 <div className="flex justify-center items-center gap-2">
