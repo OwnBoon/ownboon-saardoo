d6c70fc6ce1ddf1b80753624782fb872881f15bb
Som <aishwarysrivastava2006@gmail.com>
2023-10-21 19:14:59 +0530

    hello

--- components/Chat/ChatHeader.tsx
+++ components/Chat/ChatHeader.tsx
@@ -2,7 +2,7 @@ import IconArrowLeft from "./icon-arrow-left.svg";
 import "../../styles/chat-mobile.css";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 const ChatHeader = ({ channel, user, onBack }: any) => (
-  <div className="w-full flex  mb-2 text-center  justify-between">
+  <div className="w-full flex  fixed mb-2 text-center bg-[#151515]  justify-between">
     <button className="w-fit" onClick={onBack}>
       <ArrowLeftIcon className="h-5 w-5 ml-10 cursor-pointer" />
     </button>
