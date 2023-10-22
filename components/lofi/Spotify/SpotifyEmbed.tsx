fcba2d36e2d303d524449759176296d368e44395
lojith <lojithvinsuka1998@gmail.com>
2023-10-22 15:17:11 +0530

    position changes

--- components/lofi/Spotify/SpotifyEmbed.tsx
+++ components/lofi/Spotify/SpotifyEmbed.tsx
@@ -15,7 +15,7 @@ const SpotifyEmbed = (props: Props) => {
         <div >
             {sessionStarted && (
                 <div
-                    className={`absolute justify-center w-[300px] h-[400px] z-40 mr-[45vw] bottom-11 right-0 flex animate-slideup bg-gradient-to-br ${currentPage == '/lofi' && !hideElements ? 'block' : 'hidden'}`}>
+                    className={`absolute justify-center w-[300px] h-[400px] z-40 mr-[45vw] top-[100px] left-[100px] flex animate-slideup bg-gradient-to-br ${currentPage == '/lofi' && !hideElements ? 'block' : 'hidden'}`}>
                     {/* <MusicPlayer sessionStarted={sessionStarted} /> */}
                     <script src="https://open.spotify.com/embed/iframe-api/v1" async></script>
                     <iframe style={{ borderRadius: "12px" }} src="https://open.spotify.com/embed/artist/6XyY86QOPPrYVGvF9ch6wz?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
