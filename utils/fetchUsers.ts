10e4cefdfe9d9bdca800e89ca3e3976ffcd49d56
SaarD00ishuman <97211377+SaarD00ishuman@users.noreply.github.com>
2023-10-23 22:04:46 +0530

    e

--- utils/fetchUsers.ts
+++ utils/fetchUsers.ts
@@ -1,7 +1,7 @@
 import {  User } from '../typings'
 
 export const fetchUsers = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getUsers`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getUsers`, {next: {revalidate: 60}})
 
   const data = await res.json()
   const user: User[] = data.user
